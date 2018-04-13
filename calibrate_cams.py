Adding fix for rotary pot offset calculation in SXR line due to different in control system desing for SXR line

calibrate_cams.py
@@ -535,7 +535,11 @@ def fit_data(data, line, plot=False, verbose=False):
     fit_result, linear_fitted = cam_sinusoidal_fit(angles, linear_pot)
 
     linear_phase_offset = fit_result['phase']
-    rotary_offset = (rotary_pot[0] / avg_voltage) * gain - linear_phase_offset
+    if line == 'sxr' :
+        rotary_offset = ((rotary_pot[0]-np.min(rotary_pot)) / avg_voltage) * gain - linear_phase_offset
+    else:
+        rotary_offset = (rotary_pot[0] / avg_voltage) * gain - linear_phase_offset
+     
 
     # NOTE: octave includes a factor of 2000 below, which we removed
     linear_offset_rms_fit = np.std(linear_pot - linear_fitted)